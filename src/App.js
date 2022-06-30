1a3fbd18206deea362f2e47884b1a0829a27189b
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-06-30 19:59:30 +0530

    Add components Footer TodoItem and Todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
+import {Todos} from "./components/Todos";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
         <>
             <Header />
+            <Todos />
+            <Footer />
         </>
     );
 }
