d4776da62cf351c8d18dd16e278da03af780e635
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-01 11:13:47 +0530

    Syle footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const Footer = () => {
+    let footerStyle = {
+        position: "absolute",
+        top: "100vh",
+        width: "100%",
+    }
     return (
-        <div>
-            Footer works!
-        </div>
+        <footer className="bg-dark text-light" style={footerStyle}>
+            <p className="text-center">Copyright &copy; MyTodosList.com</p>
+        </footer>
     )
 }
